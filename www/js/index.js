e35052d366662bec5fa8eac3b39558600e0f6636
Conro <conor.wroble@gmail.com>
2017-11-27 22:52:42 -0700

    added footer buttons

--- www/js/index.js
+++ www/js/index.js
@@ -18,6 +18,21 @@ var signUpPageAlertDiv = $('#signUpPageAlertDiv');
 var loginPageAlert = $('#loginPageAlert');
 var signUpPageAlert = $('#signUpPageAlert')
 
+var mapScriptCreated = false;
+var mapElement = undefined;
+var markers = [];
+var neighborhoods = [];
+var curLatLng = undefined;
+
+//Home page vars
+var buttonHeaderLeft = $('#buttonHeaderLeft');
+var buttonHeaderRight = $('#buttonHeaderRight');
+var buttonHeaderLeftIcon = $('#buttonHeaderLeftIcon');
+var buttonHeaderRightIcon = $('#buttonHeaderRightIcon');
+var mapViewDiv = $('#mapViewDiv');
+var listViewDiv = $('#listViewDiv');
+var searchViewDiv = $('#searchViewDiv');
+
 
 /* wait until all phonegap/cordova is loaded then call onDeviceReady*/
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -27,20 +42,122 @@ function onDeviceReady(){
     console.log("JS loaded, DOC READY");
 }
 
+
+$( document ).ready(function() {
+    console.log("DOC READY");
+    mapElement = document.getElementById('mapViewDiv');
+    var map = undefined;
+    $('#userLoginInput').focus();
+
+    neighborhoods = [
+        {lat: 33.4166317, lng: -111.9341069, title: "WPC"},
+        {lat: 33.416989, lng: -111.933010, title: "WPC McCord"},
+        {lat: 33.416211, lng: -111.938084, title: "Gammage"},
+        {lat: 33.415487, lng: -111.931947, title: "ASU Gym"}
+      ];
+});
+
 /*====================*/
 /* put functions here */
 /*====================*/
 
-function initMap() {
-    var mapElement 		= document.getElementById('mapDiv');
+function drop() {
+    clearMarkers();
+    console.log(neighborhoods);
+    for (var i = 0; i < neighborhoods.length; i++) {
+        addMarkerWithTimeout(neighborhoods[i], i * 200);
+    }
+}
+
+function addMarkerWithTimeout(location, timeout) {
+    var position = {lat: location.lat, lng: location.lng};
+    var title = location.title
+    var content = title;
+    var infowindow = new google.maps.InfoWindow()
+
+
+    /*
+    console.log("What was passed through the loop: ");
+    console.log(location);
+    console.log("Position created with parameter coords: ");
+    console.log(position);
+    console.log(title);*/
     
-    var geoLocationASU 	= {lat: 33.4166317, lng: -111.9341069};
-    var mapOptions 		= {zoom: 18, center: geoLocationASU};
 
-    var mapper = new google.maps.Map(mapElement, mapOptions);
+    window.setTimeout(function() {
 
-    var markerOptions 	= {position: geoLocationASU, map: mapper};
-    var marker = new google.maps.Marker(markerOptions);
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: title,
+            animation: google.maps.Animation.DROP
+        });
+
+        markers.push(marker);
+    
+        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+            return function() {
+               infowindow.setContent(content);
+               infowindow.open(map,marker);
+            };
+        })(marker,content,infowindow)); 
+
+    }, timeout);
+
+
+
+
+}
+
+  function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers = [];
+  }
+
+function initMap() {
+    
+    //replace this with current location
+    //var geoLocationASU 	= {lat: 33.4166317, lng: -111.9341069};
+
+        console.log(curLatLng);
+
+    navigator.geolocation.getCurrentPosition(geolocationSuccess,
+        geolocationError,
+        { enableHighAccuracy: true });
+
+
+
+    map = new google.maps.Map(mapElement, {
+        zoom: 15,
+        center: curLatLng
+    });
+
+    //var mapOptions 		= {zoom: 18, center: geoLocationASU};
+
+    //var mapper = new google.maps.Map(mapElement, mapOptions);
+
+    //var markerOptions 	= {position: geoLocationASU, map: mapper};
+    //var marker = new google.maps.Marker(markerOptions);
+}
+
+function loadMapScript(callback) {
+	var script 		 = undefined;
+	var googleAPIKey = "AIzaSyBsGwlbU1AZYpGBwio6ll1kiwknKSuUIVk";
+	var googleAPIUrl = "https://maps.googleapis.com/maps/api/js";
+
+	var srcURL 		 = googleAPIUrl + '?key=' + googleAPIKey 
+							+ '&callback=' + callback;
+
+	script 			 = document.createElement('script');
+	script.type 	 = "text/javascript";
+	script.async 	 = true;
+	script.defer 	 = true;
+    script.src 		 = srcURL;
+
+    document.body.appendChild(script);
+    mapScriptCreated = true;
 }
 
 $("#signUpButton").click(function (e) { 
@@ -120,20 +237,31 @@ $("#signUpForm").submit(function(e){
 //$("#loginButton").click(function (e) { 
 $("#loginForm").submit(function (e) { 
     e.preventDefault();
+    resetAlerts();
+/*
+    $(loginPageDiv).slideUp( "slow", function() {
+        $(homePageDiv).fadeIn("fast");
+        if(!mapScriptCreated){
+            loadMapScript('initMap');
+        }
+    });*/
+    
 
+    
     login(function(success, statusMsg){
         if(success) {
             $(loginPageDiv).slideUp( "slow", function() {
                 $(homePageDiv).fadeIn("fast");
+                if(!mapScriptCreated){
+                    loadMapScript('initMap');
+                }
             });
         }
         else {
-            
+            //Create alert
+            createAlert('danger', loginPageAlertDiv, loginPageAlert, statusMsg);
         }
     });
-    //login(username, password, HelloWorld);
-
-    //Fade in/out
     
 });
 
@@ -147,29 +275,6 @@ $("#mapToLoginButton").click(function (e) {
     });
 
 });
-
-/*$("#registerButton").click(function (e) { 
-    $("#signUpForm").submit()
-    //e.preventDefault();
-
-
-    /*console.log("outPutRegistrationInfo called");
-    var name = $('#nameInput').val();
-    var username = $('#usernameInputReg').val();
-    var email = $('#emailInput').val();
-    var uni = $('#universitySelectInput').val();
-    var pass = $('#passInputReg').val();
-    var confPass = $('#passConfInputReg').val();
-    console.log(name);
-    console.log(username);
-    console.log(email);
-    console.log(uni);
-    console.log(pass);
-    console.log(confPass);
-    e.preventDefault();
-    $('#loginPageDiv').show(); 
-    $('#signUpPageDiv').hide(); 
-});*/
 
 function resetAlerts() {
     //Reset login page alert
@@ -236,3 +341,81 @@ function createAlert(type, pdiv, div, msg) {
     //Show the alert
     $(pdiv).fadeIn("fast");
 }
+
+
+$('#testButton').click(function(){
+    drop();
+});
+
+function geolocationSuccess(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+	curLatLng = new google.maps.LatLng({lat: position.coords.latitude, 
+                                        lng: position.coords.longitude});
+                                        
+    curLatLng = new google.maps.LatLng({lat: 33.4166317,
+                                        lng: -111.9341069});
+
+    //var geoLocationASU 	= {lat: 33.4166317, lng: -111.9341069};
+    mapGeolocation(curLatLng, drop);
+}
+
+function geolocationError() {
+	alert("Error in geolocation subsystem!");
+}
+
+function mapGeolocation(position, callback) {
+    map.panTo(position);
+    $('#homePageMapViewHeader').html(neighborhoods.length + " Events");
+    $('#homePageMapViewSubHeader').html("near you...");
+    $('#homePageMapViewHeader').fadeIn();
+    $('#homePageMapViewSubHeader').fadeIn();
+    callback();
+}
+
+$(buttonHeaderLeft).click(function(){
+    if($(mapViewDiv).css('display') !== 'none'){
+        $(mapViewDiv).hide();
+        $(buttonHeaderLeftIcon).toggleClass("fa-list-ul")
+        $(buttonHeaderLeftIcon).toggleClass("fa-map-o")
+        $(listViewDiv).show();
+    }
+    else{
+        $(searchViewDiv).hide();
+        $(listViewDiv).hide();
+        $(buttonHeaderLeftIcon).toggleClass("fa-map-o")
+        $(buttonHeaderLeftIcon).toggleClass("fa-list-ul")
+        $(mapViewDiv).show();
+    }
+})
+
+$(buttonHeaderRight).click(function(){
+    if($(searchViewDiv).css('display') === 'none'){
+        if($(listViewDiv).css('display') !== 'none'){
+            $(listViewDiv).hide();
+        }
+        $(mapViewDiv).hide();
+        $(buttonHeaderLeftIcon).hide();
+        $(buttonHeaderRightIcon).toggleClass("fa-search");
+        $(buttonHeaderRightIcon).toggleClass("fa-times-circle-o");
+        $(searchViewDiv).show();
+    }
+    else{
+        $(searchViewDiv).hide();
+        $(buttonHeaderLeftIcon).show();
+        $(buttonHeaderRightIcon).toggleClass("fa-times-circle-o");
+        $(buttonHeaderRightIcon).toggleClass("fa-search");
+
+        if($(buttonHeaderLeftIcon).hasClass("fa-map-o")){
+            $(listViewDiv).show();
+        }
+        else{
+            $(mapViewDiv).show();
+        }
+        //$('#mapViewDiv').show();
+    }
+})
+
+$('#testButton').click(function(){
+    console.log("testButton clicked");
+});
--- www/js/login.js
+++ www/js/login.js
@@ -7,251 +7,252 @@
 });*/
 
 function login(callback) {
-
-    //Display loading wheel 
-    $('#loginLoadingDiv').fadeIn("fast");
-
-    var success = false;
-    var statusMsg = "";
-    var username = "";
-    var password = "";
-
-    //Check if a username was entered before assigning
-    if ($('#userLoginInput').val() !== undefined){
-        username = $('#userLoginInput').val();
-    }
-
-    //Check if a password was entered before assigning
-    if ($('#passwordLoginInput').val() !== undefined){
-        password = $('#passwordLoginInput').val();
-    }
-
-    //--------------------------USING OUR API----------------------------------------------------------------------------
-    $.get("http://chollachumsapi.azurewebsites.net/users/username/" + username, function( response ) {
-        console.log(response);
-
-        //Check if results length does not equal 0 (this means user was found).
-        if (response.data.length !== 0){
-            
-            //If user was found, check if supplied password matches password stored in the database.
-            if (response.data[0].pword === password){
-                statusMsg = "Passwords matched, authorized login";
-                success = true;
+    
+        //Display loading wheel 
+        $('#loginLoadingDiv').fadeIn("fast");
+    
+        var success = false;
+        var statusMsg = "";
+        var username = "";
+        var password = "";
+    
+        //Check if a username was entered before assigning
+        if ($('#userLoginInput').val() !== undefined){
+            username = $('#userLoginInput').val();
+        }
+    
+        //Check if a password was entered before assigning
+        if ($('#passwordLoginInput').val() !== undefined){
+            password = $('#passwordLoginInput').val();
+        }
+    
+        //--------------------------USING OUR API----------------------------------------------------------------------------
+        $.get("http://chollachumsapi.azurewebsites.net/users/username/" + username, function( response ) {
+            console.log(response);
+    
+            //Check if results length does not equal 0 (this means user was found).
+            if (response.data.length !== 0){
+                
+                //If user was found, check if supplied password matches password stored in the database.
+                if (response.data[0].pword === password){
+                    statusMsg = "Passwords matched, authorized login";
+                    success = true;
+                }
+                else {
+                    statusMsg = "<b>Your login details are incorrect!</b>";
+                    success = false;
+                }
             }
+            //Results length is 0 meaning no user found by that username.
             else {
-                statusMsg = "Your login details are incorrect!";
+                statusMsg = "<b>Your login details are incorrect!</b>";
                 success = false;
             }
-        }
-        //Results length is 0 meaning no user found by that username.
-        else {
-            statusMsg = "Your login details are incorrect!";
-            success = false;
-        }
-
-        //Callback after function completes
-        callback(success, statusMsg);
-        $('#loginLoadingDiv').fadeOut("fast");
-
-      });
-
-      //--------------------------END USING OUR API----------------------------------------------------------------------------
-
-    /*
-
-    var loginQuery = "SELECT * FROM users \
-    WHERE username = '"+ username + "'";
-
-    MySql.Execute(
-        "dmazzola.com", 
-        "mweary2", 
-        "wear5238", 
-        "test_db_mweary2", 
-        loginQuery, 
-        function (data) {
-            if (data.Success){ 
-
-                //Check if results length does not equal 0 (this means user was found).
-                if (data.Result.length !== 0){
-
-                    //If user was found, check if supplied password matches password stored in the database.
-                    if (data.Result[0].pword === password){
-                        statusMsg = "Passwords matched, authorized login";
-                        success = true;
+    
+            //Callback after function completes
+            callback(success, statusMsg);
+            $('#loginLoadingDiv').fadeOut("fast");
+    
+          });
+    
+          //--------------------------END USING OUR API----------------------------------------------------------------------------
+    
+        /*
+    
+        var loginQuery = "SELECT * FROM users \
+        WHERE username = '"+ username + "'";
+    
+        MySql.Execute(
+            "dmazzola.com", 
+            "mweary2", 
+            "wear5238", 
+            "test_db_mweary2", 
+            loginQuery, 
+            function (data) {
+                if (data.Success){ 
+    
+                    //Check if results length does not equal 0 (this means user was found).
+                    if (data.Result.length !== 0){
+    
+                        //If user was found, check if supplied password matches password stored in the database.
+                        if (data.Result[0].pword === password){
+                            statusMsg = "Passwords matched, authorized login";
+                            success = true;
+                        }
+                        else {
+                            statusMsg = "Your login details are incorrect!";
+                            success = false;
+                        }
                     }
+                    //Results length is 0 meaning no user found by that username.
                     else {
                         statusMsg = "Your login details are incorrect!";
                         success = false;
                     }
                 }
-                //Results length is 0 meaning no user found by that username.
+                //Query was not successful.
                 else {
-                    statusMsg = "Your login details are incorrect!";
+                    statusMsg = "There was a problem connecting to the server!";
                     success = false;
                 }
-            }
-            //Query was not successful.
-            else {
-                statusMsg = "There was a problem connecting to the server!";
-                success = false;
-            }
-
-            //Callback after function completes
-            callback(success, statusMsg);
-            $('#loginLoadingDiv').fadeOut("fast");
-    });*/
-}
-
-function createAccount(callback) {
-
-    var statusMsg = "";
-    var user = undefined;
     
-    //$('#loginLoadingDiv').fadeIn("fast");
-
-    var nickname = $('#nickNameInputReg').val();
-    var username = $('#usernameInputReg').val();
-    var email = $('#emailInputReg').val();
-    var uni = $('#universitySelectInputReg').val();
-    var pass = $('#passInputReg').val();
-    var confPass = $('#passConfInputReg').val();
-
-    /*
-    var name = "testedfhsfhds";
-    var username = "conor111222222";
-    var email = "test";
-    var uni = "test";
-    var pass = "test";
-    var confPass = "test";
-    console.log(name);
-    console.log(username);
-    console.log(email);
-    console.log(uni);
-    console.log(pass);
-    console.log(confPass);*/
-
-    //Create object that holds the new user data
-    var user =
-    {
-        "username": username,
-        "screen_name": nickname,
-        "email": email,
-        "university": uni,
-        "pword": pass
-    };
-
-    console.log(user);
-
-    //Convert user to JSON object for the request
-    var userJson = JSON.stringify(user);
-
-    $.ajax({
-        url: 'http://chollachumsapi.azurewebsites.net/users/',
-        type: 'post',
-        data: userJson,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type':'application/json'
-        },
-        dataType: 'json',
-        success: function (data) {
-            
-            var success = data.success;
-
-            console.log(success);
-
-            if (success){
-                statusMsg = "<b>Woot! Welcome " + username + "</b>!<br>Try logging in!"
-                console.log("Woot! Welcome " + username + "!<br>Try logging in!");
-                callback(success, statusMsg, username);
-            }
-            else if (!success){
-                statusMsg = "<b>That username is already taken!</b><br>Please choose another!"
-                console.log("That username is already taken!<br>Please choose another!");
+                //Callback after function completes
                 callback(success, statusMsg);
-            }
-            else {
-                statusMsg = "Error connecting to server!"
-                console.log("Error connecting to server!");
-                callback(success, statusMsg);
-            }
-        }
-    });
-
-    /*
-    $.post( "http://chollachumsapi.azurewebsites.net/users", userJson, function( data ) {
-        console.log(data);
-      });*/
-
-
-
-   /* var insertQuery = "INSERT INTO users (username, screen_name, email, university, pword) \
-    SELECT * FROM (SELECT '" + username + "' as username, \
-                            '" + name + "' as screen_name,\
-                            '" + email + "' as email,\
-                            '" + uni  + "' as university,\
-                            '" + pass + "' as password) as tmp\
-    WHERE NOT EXISTS (\
-        SELECT username\
-        FROM users\
-        WHERE username = 'conor') LIMIT 1;"
-
-
-
+                $('#loginLoadingDiv').fadeOut("fast");
+        });*/
+    }
     
-    var existsQuery = "SELECT username FROM users \
-    WHERE username = '"+ username + "' \
-    AND email = '"+ email + "'";
-
-    var insertQuery = "INSERT INTO test_db_mweary2.users (username, screen_name, email, university, pword)\
-    VALUES ('" + username + "', '" + name + "', '" + email + "', '" + uni + "', '" + pass + "');"
-
-    MySql.Execute(
-        "dmazzola.com", 
-        "mweary2", 
-        "wear5238", 
-        "test_db_mweary2",
-        insertQuery, 
-        function (data) {
-            console.log(data);
-            if (data.Success){ 
+    function createAccount(callback) {
+    
+        var statusMsg = "";
+        var user = undefined;
+        
+        //$('#loginLoadingDiv').fadeIn("fast");
+    
+        var nickname = $('#nickNameInputReg').val();
+        var username = $('#usernameInputReg').val();
+        var email = $('#emailInputReg').val();
+        var uni = $('#universitySelectInputReg').val();
+        var pass = $('#passInputReg').val();
+        var confPass = $('#passConfInputReg').val();
+    
+        /*
+        var name = "testedfhsfhds";
+        var username = "conor111222222";
+        var email = "test";
+        var uni = "test";
+        var pass = "test";
+        var confPass = "test";
+        console.log(name);
+        console.log(username);
+        console.log(email);
+        console.log(uni);
+        console.log(pass);
+        console.log(confPass);*/
+    
+        //Create object that holds the new user data
+        var user =
+        {
+            "username": username,
+            "screen_name": nickname,
+            "email": email,
+            "university": uni,
+            "pword": pass
+        };
+    
+        console.log(user);
+    
+        //Convert user to JSON object for the request
+        var userJson = JSON.stringify(user);
+    
+        $.ajax({
+            url: 'http://chollachumsapi.azurewebsites.net/users/',
+            type: 'post',
+            data: userJson,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type':'application/json'
+            },
+            dataType: 'json',
+            success: function (data) {
                 
-                //Check if results length does not equal 0 (this means user was found).
-                if (data.Result.length  === 0){
-                    statusMsg = "Account doesn't exist!";
-                    console.log(statusMsg);
-
-                    
-
-                    success = true;
+                var success = data.success;
+    
+                console.log(success);
+    
+                if (success){
+                    statusMsg = "<b>Woot! Welcome " + username + "</b>!<br>Try logging in!"
+                    console.log("Woot! Welcome " + username + "!<br>Try logging in!");
+                    callback(success, statusMsg, username);
                 }
-                //Results length is 0 meaning no user found by that username.
+                else if (!success){
+                    statusMsg = "<b>That username is already taken!</b><br>Please choose another!"
+                    console.log("That username is already taken!<br>Please choose another!");
+                    callback(success, statusMsg);
+                }
                 else {
-                    //statusMsg = "Account already exists!";
-                    //console.log(statusMsg);
-                    //success = false;
+                    statusMsg = "Error connecting to server!"
+                    console.log("Error connecting to server!");
+                    callback(success, statusMsg);
                 }
             }
-            else {
-                statusMsg = "Account already exists";
-                console.log(statusMsg);
-                success = false;
-            }
-
-            //Callback after function completes
-            callback(success, statusMsg);
-            $('#loginLoadingDiv').fadeOut("fast");
-    });
-
-    */
-}
-
-function setCurrentUser() {
-
-}
-
-function getCurrentUser() {
-
-}
-
-
+        });
+    
+        /*
+        $.post( "http://chollachumsapi.azurewebsites.net/users", userJson, function( data ) {
+            console.log(data);
+          });*/
+    
+    
+    
+       /* var insertQuery = "INSERT INTO users (username, screen_name, email, university, pword) \
+        SELECT * FROM (SELECT '" + username + "' as username, \
+                                '" + name + "' as screen_name,\
+                                '" + email + "' as email,\
+                                '" + uni  + "' as university,\
+                                '" + pass + "' as password) as tmp\
+        WHERE NOT EXISTS (\
+            SELECT username\
+            FROM users\
+            WHERE username = 'conor') LIMIT 1;"
+    
+    
+    
+        
+        var existsQuery = "SELECT username FROM users \
+        WHERE username = '"+ username + "' \
+        AND email = '"+ email + "'";
+    
+        var insertQuery = "INSERT INTO test_db_mweary2.users (username, screen_name, email, university, pword)\
+        VALUES ('" + username + "', '" + name + "', '" + email + "', '" + uni + "', '" + pass + "');"
+    
+        MySql.Execute(
+            "dmazzola.com", 
+            "mweary2", 
+            "wear5238", 
+            "test_db_mweary2",
+            insertQuery, 
+            function (data) {
+                console.log(data);
+                if (data.Success){ 
+                    
+                    //Check if results length does not equal 0 (this means user was found).
+                    if (data.Result.length  === 0){
+                        statusMsg = "Account doesn't exist!";
+                        console.log(statusMsg);
+    
+                        
+    
+                        success = true;
+                    }
+                    //Results length is 0 meaning no user found by that username.
+                    else {
+                        //statusMsg = "Account already exists!";
+                        //console.log(statusMsg);
+                        //success = false;
+                    }
+                }
+                else {
+                    statusMsg = "Account already exists";
+                    console.log(statusMsg);
+                    success = false;
+                }
+    
+                //Callback after function completes
+                callback(success, statusMsg);
+                $('#loginLoadingDiv').fadeOut("fast");
+        });
+    
+        */
+    }
+    
+    function setCurrentUser() {
+    
+    }
+    
+    function getCurrentUser() {
+    
+    }
+    
+    
+    
